f87328859d5973fc6288d914fd98e4ea14b54c2e
Stine Losnedahl <93385431+slosne@users.noreply.github.com>
2023-03-25 14:44:20 +0100

    References updated

--- src/App.js
+++ src/App.js
@@ -61,3 +61,41 @@ function App() {
 }
 
 export default App;
+
+/* Kilder
+
+ackarlse for GitHub(24 februar, 2023). Forelesning om api og søkefunksjon fra kode "gitcollab".
+https://github.com/ackarlse/gitcollab/commits/main
+
+Christian Robertson for Google Fonts(u.å). Roboto Condensed.
+https://fonts.google.com/specimen/Roboto+Condensed 
+
+Font Awesome(u.å). 
+https://fontawesome.com/icons/magnifying-glass?f=classic&s=regular
+
+Fritz, B.(u.å). OMDb API.
+https://www.omdbapi.com/ 
+
+Liu, J.(4 november, 2019). How to use async/await with map and Promise.all.
+https://dev.to/jamesliudotcc/how-to-use-async-await-with-map-and-promise-all-1gb5 
+
+Myke_simon for Unsplash(8. september, 2018). Movie Night.
+https://unsplash.com/photos/atsUqIm3wxo 
+
+Popeye for StackOverflow(8. september, 2017). Is it possible to map only a portion of an array? (Array.map())
+https://stackoverflow.com/questions/39455224/is-it-possible-to-map-only-a-portion-of-an-array-array-map
+
+Schäferhoff, N(8. mars, 2022). CSS Box Shadow Tutorial: A Step-By-Step Guide (+ Examples).
+https://torquemag.io/2022/03/css-box-shadow-tutorial/
+
+Schwarzmüller, M. for Udemy(mars, 2023). React - The Complete Guide (incl Hooks, React Router, Redux).
+https://www.udemy.com/course/react-the-complete-guide-incl-redux/ 
+
+W2schools(u.å). Sass @import and Partials.
+https://www.w3schools.com/sass/sass_import.php 
+
+W3schools(u.å). Sass @mixin and @include.
+https://www.w3schools.com/sass/sass_mixin_include.php
+
+
+*/
